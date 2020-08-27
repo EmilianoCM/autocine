6ada5b2dd822ff23b6c2aa405bf611424729adf9
facubarberis <facubarberis@gmail.com>
2020-08-27 10:12:20 -0300

    login adaptacion

--- lib/src/utils/rutas.dart
+++ lib/src/utils/rutas.dart
@@ -1,3 +1,6 @@
+import 'dart:js';
+
+import 'package:autocinefsa/src/pages/Login/login_page.dart';
 import 'package:flutter/widgets.dart';
 import 'package:autocinefsa/src/pages/Drawer_menu/drawer_page.dart';
 import 'package:autocinefsa/src/pages/Home/home_page.dart';
@@ -6,5 +9,6 @@ import 'package:autocinefsa/src/pages/splash/splash_page.dart';
 final Map<String, WidgetBuilder> rutas = {
   SplashPage.routeName: (context) => SplashPage(),
   HomePage.routeName: (context) => HomePage(),
-  DrawerMenu.routeName: (context) => DrawerMenu()
+  DrawerMenu.routeName: (context) => DrawerMenu(),
+  LoginPage.routeName: (context) => LoginPage(),
 };
