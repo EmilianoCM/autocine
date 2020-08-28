283b4dfb8d528cea9eb5c9d105c4a958e061c871
facubarberis <facubarberis@gmail.com>
2020-08-28 08:17:47 -0300

    Comentarios

--- lib/src/utils/rutas.dart
+++ lib/src/utils/rutas.dart
@@ -4,6 +4,13 @@ import 'package:autocinefsa/src/pages/Drawer_menu/drawer_page.dart';
 import 'package:autocinefsa/src/pages/Home/home_page.dart';
 import 'package:autocinefsa/src/pages/splash/splash_page.dart';
 
+/*Para para el redireccionamiento hacia las  paginas Requeridas
+//Todas las clases devuelven un Scaffold, que a la vez contiene 
+una funcion Body particular de cada clase, definida en otro archivo
+que se encuentra dentro de la carpeta COMPONENTS dentro de cada una 
+de las carpetas de las clases especificamente
+*/
+
 final Map<String, WidgetBuilder> rutas = {
   SplashPage.routeName: (context) => SplashPage(),
   HomePage.routeName: (context) => HomePage(),
